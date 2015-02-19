88bb6622d7e54c87cbf0ad83bef0f4caabc26eb7
VictorRiosM <victor.riosm@live.com>
2015-02-19 17:23:48 -0600

    Caja envolvente

--- shapesdetector.py
+++ shapesdetector.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python2
-import Image
+import Image, ImageDraw
 from sys import argv
 from imageprocessing import openImage
 from imageprocessing import neighborhood
 
+def drawbox(newimage, figure):
+   x = []
+   y = []
+   for i in figure:
+      x.append(i[0])
+      y.append(i[1])
+   minx = min(x)
+   miny = min(y)
+   maxx = max(x)
+   maxy = max(y)
+   draw = ImageDraw.Draw(newimage)
+   draw.rectangle((minx, miny, maxx, maxy), outline = 'green')
+   return newimage
+
 def detectshapes(image):
    pixels = image.load()
    width, height = image.size
+   #newimage=Image.new('RGB', (width, height))
+   newimage = image.copy()
    visited = []
    nshapes = 0
    i=0
@@ -21,7 +37,6 @@ def detectshapes(image):
             #DFS
             s = []
             s_visited = []
-            
             s.append((x, y))
             while len(s) > 0:
                cur = s.pop()
@@ -32,11 +47,12 @@ def detectshapes(image):
                         if nb[0] >= 0 and nb[1] >= 0 and nb[0] < width-1 and nb[1] < height-1:
                            if pixels[nb] != (0, 0, 0):
                               s.append(nb)
-                     
                   s_visited.append(cur)
+            newimage = drawbox(newimage, s_visited)
             visited.extend(s_visited)
          visited.append((x, y))
    print i
+   newimage.save('newimage.png')
    print len(visited)
    print width, height, width*height, nshapes
 
