f4cdb01c689029f063bbfbf33bc7c98c7d0b9c84
VictorRiosM <victor.riosm@live.com>
2015-02-20 01:52:29 -0600

    Percentages

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ The first work is an edge detector. The edgedetector.py file contains the code f
 
 ##Shapes detection
 The second work is a shapes detector. shapesdetector.py is the script that detects shapes.
+
+The script receives two parameters: the input image and the output image. If the arguments are not given the script displays a window to select the input image.
+
+The output:
+- All the shapes and the background are randomly colored.
+- Each shape has its own bounding box. The color of the box is the same of the shape.
+- The white lines are the borders of a shape.
+- A yellow point indicates the bounding box center.
+- A black point indicates the mass center. Note: the yellow points can overwrite the black points.
+- The script yields the image space percentage used by each shape and the background.
+
+The folder 'Shapesdetection' contains some images in which the script has been used.
--- shapesdetector.py
+++ shapesdetector.py
@@ -5,20 +5,32 @@ from imageprocessing import openImage
 import colorsys
 from random import uniform
 
-def center(x, y):
+# Bounding box center
+def boxcenter(x, y):
    xmed = sum(x)/len(x)
    ymed = sum(y)/len(y)
    return xmed, ymed
 
-# color() returns a random color
+# Mass center
+def getmasscenter(shape):
+   x = []
+   y = []
+   for p in shape:
+      x.append(p[0]) 
+      y.append(p[1])
+   xprom = sum(x)/len(x)
+   yprom = sum(y)/len(y)
+   return xprom, yprom
+
+# Returns a random color
 def color():
    # The hue value is randomized
    hsv = uniform(0, 1), 1.0, 1.0
    r, g, b = colorsys.hsv_to_rgb(*hsv)
    return int(r*255), int(g*255), int(b*255)
 
-# Draw the bounding box and get the mass center
-def drawbox(newimage, figure, col):
+# Draw the bounding box and the mass center
+def drawbox(newimage, figure, col, mcenter):
    x = []
    y = []
    for i in figure:
@@ -30,9 +42,14 @@ def drawbox(newimage, figure, col):
    maxy = max(y)
    draw = ImageDraw.Draw(newimage)
    draw.rectangle((minx, miny, maxx, maxy), outline = col)
-   c = center(x, y)
-   draw.point(c, fill = col)
+   # Mass center Black
+   draw.point(mcenter, fill = 'black')
+   # Bounding box center Yellow
+   draw.point(boxcenter(x, y), fill = 'yellow')
    return newimage
+
+def getpercentage(shape, width, height):
+   return 100.0/float(width*height) * len(shape)
 
 # The depth first search subroutine
 def dfs((x, y), pixels, visited, width, height):
@@ -51,44 +68,56 @@ def dfs((x, y), pixels, visited, width, height):
          s_visited.append(cur)
    return s_visited
 
+#Floodfill subroutine
+# Fills every shape with a different color
 def floodfill(pixels, p, width, height):
    s = []
    newcolor = color()
    if pixels[p] != (0, 0, 0):
-      return pixels, None
+      return pixels, None, None
    s.append(p)
+   shape = []
    while len(s) != 0:
       p = s.pop()
       if pixels[p] == (0, 0, 0):
          pixels[p] = newcolor
+         shape.append(p)
          x, y = p
-         if x-1>=0 and pixels[x-1, y] == (0, 0, 0):
+         # 4-neighborhood, (0, 0, 0) means that the pixel has not been filled
+         if x-1>=0 and (pixels[x-1, y] == (0, 0, 0) or pixels[x-1, y] == (255, 255, 255)):
             s.append((x-1, y))
-         if x+1<width and pixels[x+1, y] == (0, 0, 0):
+         if x+1<width and (pixels[x+1, y] == (0, 0, 0) or pixels[x+1, y] == (255, 255, 255)):
             s.append((x+1, y))
-         if y-1>=0 and pixels[x, y-1] == (0, 0, 0):
+         if y-1>=0 and (pixels[x, y-1] == (0, 0, 0) or pixels[x, y-1] == (255, 255, 255)):
             s.append((x, y-1))
-         if y+1<height and pixels[x, y+1] == (0, 0, 0):
+         if y+1<height and (pixels[x, y+1] == (0, 0, 0) or pixels[x, y+1] == (255, 255, 255)):
             s.append((x, y+1))
-   return pixels, newcolor
+   return pixels, newcolor, shape
 
+
+# The main subroutine for shapedetection
 def detectshapes(image, path):
    pixels = image.load()
    width, height = image.size
-   
+
+   # Data received from the floodfill
    colors = []
+   mcenters = []
+   percentages = []
+   # Apply floodfill
    for x in xrange(0, width):
       for y in xrange(0, height):
-         pixels, color = floodfill(pixels, (x, y), width, height)
+         pixels, color, shape = floodfill(pixels, (x, y), width, height)
          if color != None:
             colors.append(color)
+         if shape != None:
+            mcenters.append(getmasscenter(shape))
+            percentages.append(getpercentage(shape, width, height))
    i = Image.new('RGB', (width, height))
    for x in xrange(0, width):
       for y in xrange(0, height):
          i.putpixel((x, y), pixels[x, y])
-   #i.save('Shapesdetection/floodfill.png')
    
-   # New image to show the result   
    visited = [] # List to save the visited pixels
    nshapes=0
    for x in xrange(0, width):
@@ -99,11 +128,18 @@ def detectshapes(image, path):
             # The dfs subroutine returns the shape edge pixels
             s_visited = dfs((x, y), pixels, visited, width, height)
             # Draw the box and the mass center
-            i = drawbox(i, s_visited, colors[nshapes+1])
+            i = drawbox(i, s_visited, colors[nshapes], mcenters[nshapes])
             visited.extend(s_visited)
          visited.append((x, y))
-   #newimage.save(path)
    i.save(path)
+   pbgcolor = max(percentages)
+   for n in xrange(0, nshapes):
+      if percentages[n] == pbgcolor:
+         print "Element:", n, "Color:", colors[n], "Percentage:", percentages[n], "Probably this is the background"
+      else:
+         print "Element:", n, "Color:", colors[n], "Percentage:", percentages[n]
+
+
 
 if __name__=='__main__':
    try:
